769cd1339ef39096a3c73182d57015cd2bc185d7
nikhil-verma12 <nikhilverma22012007@gmail.com>
2026-02-20 20:31:46 +0530

    Implement Baseball Game score calculation

---
+++ Stack/Baseball_Game.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int calPoints(String[] arr) {
+        int size = arr.length;
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0 ; i<size ; i++){
+            String s = arr[i];
+            if(s.equals("C")){
+                st.pop();
+            }
+            else if(s.equals("D")){
+                st.push(2*st.peek());
+            }
+            else if(s.equals("+")){
+                int top = st.pop();
+                int secondTop = st.peek();
+                int sum = top + secondTop;
+                st.push(top);
+                st.push(sum);
+
+            }
+            else{
+                st.push(Integer.parseInt(s));
+            }
+        }
+        int sum = 0;
+        while(!st.isEmpty()){
+            sum += st.pop();
+        }
+        return sum;
+    }
+}
