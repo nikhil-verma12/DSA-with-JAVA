dfb2dea070dbb76765002ce26dfe000ba02cddfb
nikhil-verma12 <nikhilverma22012007@gmail.com>
2026-04-08 22:07:25 +0530

    Implement Morris Traversal for BST validation
    Refactor isValidBST method to use Morris Traversal for in-order traversal without recursion or stack.

--- Binary Tree/Validate_BST.java
+++ Binary Tree/Validate_BST.java
@@ -13,18 +13,52 @@
  *     }
  * }
  */
+// --------------------------------------------------------------------------------------------------------------------------------------------
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         if(root == null) return true;
+//         return isValid(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        
+//     }
+//     public boolean isValid(TreeNode root,long min,long max){
+//         if(root == null) return true;
+//         else if(root.val <= min || root.val >= max){
+//             return false;
+//         }
+//         return isValid(root.left,min,root.val) && isValid(root.right,root.val,max);
+
+//     }
+// }
+// -------------------------------------------------------------------------------------------------------------------------------------------
+
+// Morris Traversal---------------------------------------------------------------------------------------------------------------------------
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        if(root == null) return true;
-        return isValid(root,Long.MIN_VALUE,Long.MAX_VALUE);
-        
-    }
-    public boolean isValid(TreeNode root,long min,long max){
-        if(root == null) return true;
-        else if(root.val <= min || root.val >= max){
-            return false;
+        TreeNode curr = root;
+        long prev = Long.MIN_VALUE;
+        while(curr != null ){
+            if(curr.left != null){
+                TreeNode pred = curr.left;
+                while(pred.right != null && pred.right != curr){
+                    pred = pred.right;
+                }
+                if(pred.right == null){
+                    pred.right = curr;
+                    curr = curr.left;
+                }
+                else{
+                    pred.right = null;
+                    if(curr.val <= prev) return false;
+                    prev = curr.val;
+                    curr = curr.right;
+                }
+            }
+            else{
+                if(curr.val <= prev) return false;
+                prev = curr.val;
+                curr = curr.right;   
+            }
         }
-        return isValid(root.left,min,root.val) && isValid(root.right,root.val,max);
-
+        return true;
     }
 }
